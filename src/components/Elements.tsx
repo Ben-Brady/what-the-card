366fcded0c9064ae0b0f3ce87b9e58f38a0130f9
Ben Brady <benbrady04.bb@gmail.com>
2025-05-12 17:51:49 +0100

    feat: updated button colours

--- src/components/Elements.tsx
+++ src/components/Elements.tsx
@@ -1,12 +1,12 @@
 import { classed } from "@tw-classed/solid";
 
 export const Button = classed.button(
-    "content-center text-center px-4 py-2 rounded-lg text-2xl cursor-pointer disabled:opacity-60",
+    "w-full max-w-80 content-center text-center px-4 py-2 rounded-lg text-2xl cursor-pointer disabled:opacity-60 min-h-14 h-fit",
     {
         variants: {
             variant: {
                 primary: "bg-blue-600",
-                secondary: "bg-[#133f9e] text-[#ebf4ff]",
+                secondary: "bg-blue-500",
                 destructive: "bg-red-500",
             },
         },
@@ -14,21 +14,21 @@ export const Button = classed.button(
 );
 
 export const LinkButton = classed.a(
-    "content-center text-center px-4 py-2 rounded-lg text-2xl cursor-pointer",
+    "w-full max-w-80 content-center text-center px-4 py-2 rounded-lg text-2xl cursor-pointer min-h-14 h-fit",
     {
         variants: {
             variant: {
                 primary: "bg-blue-600",
-                secondary: "bg-[#133f9e] text-[#ebf4ff]",
+                secondary: "bg-blue-500",
             },
         },
     },
 );
 
-export const Input = classed.input(
-    "px-2 py-1 text-xl h-fit bg-white rounded-md text-black outline-none",
-);
-
 export const Textarea = classed.textarea(
     "px-2 py-1 h-fit bg-white rounded-md text-black resize-none w-full outline-none",
+);
+
+export const Input = classed.input(
+    "px-2 py-1 text-xl h-fit bg-white rounded-md text-black outline-none",
 );
