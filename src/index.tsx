f0f10a05db8b68356041402c115019f7783d3d43
Ben Brady <benbrady04.bb@gmail.com>
2025-05-14 14:13:15 +0100

    feat: updated /about page routes

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import { render } from "solid-js/web";
 import { Route, Router } from "@solidjs/router";
 import Layout from "@/pages/+layout.tsx";
 import HomePage from "@/pages/home.tsx";
-import PackPage from "@/pages/game/pack.tsx";
 import PlayPage from "@/pages/play.tsx";
 import PacksPage from "@/pages/packs.tsx";
+import PackPage from "@/pages/game/pack.tsx";
+import GamePage from "@/pages/game/play.tsx";
+import AboutPage from "@/pages/about/about.tsx";
 import CreditsPage from "@/pages/about/credits.tsx";
 import "./styles/index.css";
 
 const CreatePage = lazy(() => import("@/pages/create.tsx"));
-const AboutPage = lazy(() => import("@/pages/about/about.tsx"));
 const TermsOfServicePage = lazy(() => import("@/pages/about/tos.tsx"));
 const PrivacyPolicyPage = lazy(() => import("@/pages/about/privacy.tsx"));
-const GamePage = lazy(() => import("@/pages/game/play.tsx"));
 
 render(
     () => (
@@ -30,10 +30,10 @@ render(
 
             {/* Abort Pages */}
             <Route path="/about" component={AboutPage} />
-            <Route path="/privacy" component={PrivacyPolicyPage} />
-            <Route path="/tos" component={TermsOfServicePage} />
-            <Route path="/gdpr" component={TermsOfServicePage} />
-            <Route path="/credits" component={CreditsPage} />
+            <Route path="/about/credits" component={CreditsPage} />
+            <Route path="/about/privacy" component={PrivacyPolicyPage} />
+            <Route path="/about/tos" component={TermsOfServicePage} />
+            <Route path="/about/gdpr" component={TermsOfServicePage} />
         </Router>
     ),
     document.getElementById("root")!,
--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -10,16 +10,16 @@ export default function AboutPage() {
             )}
         >
             <div class="size-full flex flex-col items-center gap-4">
-                <LinkButton variant="secondary" href="/credits" preload>
+                <LinkButton variant="secondary" href="/about/credits" preload>
                     Credits
                 </LinkButton>
-                <LinkButton variant="secondary" href="/tos" preload>
+                <LinkButton variant="secondary" href="/about/tos" preload>
                     Terms of Service
                 </LinkButton>
-                <LinkButton variant="secondary" href="/privacy" preload>
+                <LinkButton variant="secondary" href="/about/privacy" preload>
                     Privacy Policy
                 </LinkButton>
-                <LinkButton variant="secondary" href="/gdpr" preload>
+                <LinkButton variant="secondary" href="/about/gdpr" preload>
                     GDPR
                 </LinkButton>
             </div>
