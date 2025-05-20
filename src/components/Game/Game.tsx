7413055de2e62c0c51fd0ec71f661359e61aa6e5
Ben Brady <benbrady04.bb@gmail.com>
2025-05-20 22:13:23 +0100

    feat(Game): removed goBack optionality

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -8,24 +8,22 @@ export const Game = (props: { cards: Card[] }) => {
     const cards = () => props.cards;
     const { card, progress, direction, goBack, goNext } = useGame(cards());
 
-    const isFirst = () => progress() === 0;
-
     return (
         <>
             <ProgressBar progress={progress} />
             <CardElement card={card} direction={direction} />
-            <TapOverlay onNext={goNext} onBack={isFirst() ? undefined : goBack} />
+            <TapOverlay onNext={goNext} onBack={goBack} />
         </>
     );
 };
 
-const TapOverlay = (props: { onNext: () => void; onBack?: () => void }) => (
+const TapOverlay = (props: { onNext: () => void; onBack: () => void }) => (
     <>
         <div
             class="absolute left-0 top-0 bottom-0 z-10 w-1/2"
-            onPointerUp={() => props.onBack?.()}
-            onTouchEnd={() => props.onBack?.()}
-            onTouchCancel={() => props.onBack?.()}
+            onPointerUp={() => props.onBack()}
+            onTouchEnd={() => props.onBack()}
+            onTouchCancel={() => props.onBack()}
         />
         <div
             class="absolute right-0 top-0 bottom-0 z-10 w-1/2"
