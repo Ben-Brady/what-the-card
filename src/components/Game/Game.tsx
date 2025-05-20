095c43aa098bedc637ef465ccebff64a46752e48
Ben Brady <benbrady04.bb@gmail.com>
2025-05-20 22:06:36 +0100

    feat(Game): updated tap overlay

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,11 +1,8 @@
 import "./Game.css";
-import { Show } from "solid-js";
 import { useGame } from "@/hooks/useGame";
 import { Card } from "@/lib/pack";
 import CardElement from "./CardElement";
 import ProgressBar from "./ProgressBar";
-import { useHolding } from "@/hooks/useHolding";
-import classNames from "@/lib/classnames";
 
 export const Game = (props: { cards: Card[] }) => {
     const cards = () => props.cards;
@@ -16,39 +13,25 @@ export const Game = (props: { cards: Card[] }) => {
     return (
         <>
             <ProgressBar progress={progress} />
+            <CardElement card={card} direction={direction} />
             <TapOverlay onNext={goNext} onBack={isFirst() ? undefined : goBack} />
-
-            <div class="absolute inset-0 bg-blue-300 contain-strict">
-                <CardElement card={card} direction={direction} />
-            </div>
         </>
     );
 };
 
-const TapOverlay = (props: { onNext: () => void; onBack?: () => void }) => {
-    const isHolding = useHolding();
-
-    return (
+const TapOverlay = (props: { onNext: () => void; onBack?: () => void }) => (
+    <>
         <div
-            class={classNames(
-                "absolute inset-0 contain-strict z-10 duration-75",
-                isHolding() ? "opacity-20" : "opacity-0",
-            )}
-        >
-            <Show when={props.onBack}>
-                <div
-                    class="absolute w-1/3 left-0 top-0 bottom-0 bg-red-500"
-                    onPointerUp={() => props.onBack?.()}
-                    onTouchEnd={() => props.onBack?.()}
-                    onTouchCancel={() => props.onBack?.()}
-                />
-            </Show>
-            <div
-                class="absolute w-1/3 right-0 top-0 bottom-0 bg-green-500"
-                onPointerUp={() => props.onNext()}
-                onTouchEnd={() => props.onNext()}
-                onTouchCancel={() => props.onNext()}
-            />
-        </div>
-    );
-};
+            class="absolute left-0 top-0 bottom-0 z-10 w-1/2"
+            onPointerUp={() => props.onBack?.()}
+            onTouchEnd={() => props.onBack?.()}
+            onTouchCancel={() => props.onBack?.()}
+        />
+        <div
+            class="absolute right-0 top-0 bottom-0 z-10 w-1/2"
+            onPointerUp={() => props.onNext()}
+            onTouchEnd={() => props.onNext()}
+            onTouchCancel={() => props.onNext()}
+        />
+    </>
+);
