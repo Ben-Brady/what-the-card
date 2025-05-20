937b8bbb04a93e7d87562982c6c5603395c9eb84
Ben Brady <benbrady04.bb@gmail.com>
2025-05-20 20:36:38 +0100

    version 1.2.2

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -1,7 +1,7 @@
 import classNames from "@/lib/classnames";
 import { ComponentProps } from "solid-js";
 
-const VERSION = "1.2.1";
+const VERSION = "1.2.2";
 
 export default function HomeLayout(props: ComponentProps<"div">) {
     return (
