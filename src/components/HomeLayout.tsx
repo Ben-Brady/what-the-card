5541f730b5287d73f25e6a51849262b7acd99590
Ben Brady <benbrady04.bb@gmail.com>
2025-06-04 20:34:11 +0100

    feat: removed default transition-down

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -33,14 +33,10 @@ export default function HomeLayout(props: { children: JSXElement }) {
     return (
         <div
             id="home-layout"
-            class={classNames(
-                "size-full flex justify-center overflow-y-auto",
-                `depth-${depth()} transition-down`,
-                {
-                    "transition-up": direction() === "up",
-                    "transition-down": direction() === "down",
-                },
-            )}
+            class={classNames("size-full flex justify-center overflow-y-auto", `depth-${depth()}`, {
+                "transition-up": direction() === "up",
+                "transition-down": direction() === "down",
+            })}
         >
             <div class="absolute bottom-0 right-1 text-neutral-600">{VERSION}</div>
             <div class="size-full max-w-160 p-8 relative flex flex-col items-center justify-between gap-4">
@@ -54,7 +50,7 @@ const depths: Record<string, number> = {
     "/": 1,
     "/play": 2,
     "/packs": 2,
-    "/custom": 2,
+    "/cards": 2,
     "/about": 2,
     "/about/changelog": 3,
     "/about/credits": 3,
