e72698a95025b506f82b16d388c58347e02dbb63
Ben Brady <benbrady04.bb@gmail.com>
2025-06-08 17:24:13 +0100

    fix(cards): fixed view transition

--- src/pages/cards.tsx
+++ src/pages/cards.tsx
@@ -1,16 +1,21 @@
-import { createSignal, Show } from "solid-js";
+import { createEffect, createSignal, Show } from "solid-js";
 import HomeLayout from "@/components/HomeLayout";
 import TabSelect from "@/components/TabSelect";
 import BuiltinCards from "@/components/CardsPage/BuiltInCards";
 import CustomCards from "@/components/CardsPage/CustomCards";
 import { transition } from "@/lib/transition";
 import classNames from "@/lib/classnames";
+import { useBeforeLeave } from "@solidjs/router";
 
 export default function CardsPage() {
     type Tab = "Built-in" | "Custom";
     const tabs: Tab[] = ["Built-in", "Custom"];
     const [currentTab, setCurrentTab] = createSignal<Tab>("Built-in");
     const [animation, setAnimation] = createSignal<"right" | "left" | "none">("none");
+
+    useBeforeLeave(() => {
+        setAnimation("none");
+    });
 
     return (
         <>
@@ -19,11 +24,12 @@ export default function CardsPage() {
                     currentTab={currentTab}
                     setTab={(v) => {
                         setAnimation(v === "Built-in" ? "left" : "right");
-                        transition(() => setCurrentTab(v)).then(() => setAnimation("none"));
+                        transition(() => setCurrentTab(v));
                     }}
                     tabs={tabs}
                     style={{ "view-transition-name": "cards-tab-select" }}
                 />
+
                 <div
                     class={classNames(
                         "w-full flex flex-col items-center justify-between gap-4",
