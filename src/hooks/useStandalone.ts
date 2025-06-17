cee5f4154d10e4a41ce92ba1e2a9eb8e6debefd3
Ben Brady <benbrady04.bb@gmail.com>
2025-06-17 12:31:09 +0100

    refactor(useStandalone): inlined function

--- src/hooks/useStandalone.ts
+++ src/hooks/useStandalone.ts
@@ -4,9 +4,6 @@ const query = window.matchMedia("(display-mode: standalone)");
 
 export const useStandalone = () => {
     const [standalone, setStandalone] = createSignal(query.matches);
-    query.addEventListener("change", (e) => {
-        setStandalone(e.matches);
-    });
-
+    query.addEventListener("change", (e) => setStandalone(e.matches));
     return standalone;
 };
